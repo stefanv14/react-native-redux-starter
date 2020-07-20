c586e88cd4ba7c06f821d32196c248c0fa81c522
= <vasilijevic032@gmail.com>
2020-07-20 17:29:11 +0200

    starter commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import globalReducer from './Redux/Reducers/global';
+import InitialScreen from './src/screens/InitialScreen';
 
-export default function App() {
-  return (
+const rootReducer = combineReducers({
+  global: globalReducer,
+});
+
+const store = createStore(rootReducer);
+
+const App = () => (
+  <Provider store={store}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <InitialScreen />
     </View>
-  );
-}
+  </Provider>
+);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
